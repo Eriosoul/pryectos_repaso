9c5f760b6b94584e4574c34bc36372d45c1a0b7f
Eriosoul <tboss1780@gmail.com>
2024-10-26 20:42:41 +0200

    se elimina el producto correctamente

--- To-Dolist/main.py
+++ To-Dolist/main.py
@@ -6,33 +6,42 @@ def prod():
     return prods
 prod()
 
-def new_prod():
-    title = input("Nombre: ")
-    description = input("Definicion: ")
-    done = input("Terminado: ")
-    ultimo_producto = products[-1]['id']
-    nuevo_id = ultimo_producto +1
-    nuevo_prod = {
-        'id': nuevo_id,
-        'title': title,
-        'description': description,
-        'done': done
-    }
-    products.append(nuevo_prod)
-    print(products)
+# def new_prod():
+#     title = input("Nombre: ")
+#     description = input("Definicion: ")
+#     done = input("Terminado: ")
+#     ultimo_producto = products[-1]['id']
+#     nuevo_id = ultimo_producto +1
+#     nuevo_prod = {
+#         'id': nuevo_id,
+#         'title': title,
+#         'description': description,
+#         'done': done
+#     }
+#     products.append(nuevo_prod)
+#     print(products)
 
-new_prod()
+# new_prod()
 
 
 def kit_prod():
     print(products)
-    num_elim = input(str("Introduce el id que deseas eliminar: "))
-    print(f"Ha selecionado el {num_elim} ",)
-    opcion = input("¿Es correcto?  Y(Si)/N(no)").title()
+    num_elim = int(input("Introduce el id que deseas eliminar: "))  # Convertimos a entero
+    print(f"Ha seleccionado el {num_elim}")
+    opcion = input("¿Es correcto? Y(Si)/N(no)").title()
     print(opcion)
+    
     if opcion == "Y":
-        print("Se elminino correctamente")
+        for product in products:
+            if product['id'] == num_elim:
+                products.remove(product)
+                print("Se elimino correctamnete el producto", product)
+            
+            else:
+                print("No se encontró un producto con ese id.")
     else:
-        print("No se elemino nada")
+        print("No se eliminó nada")
 
 kit_prod()
+prod()
+
