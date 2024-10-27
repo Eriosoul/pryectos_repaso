5b871bafee2ffcf3906006ce4b8e1fca49f7d51a
Eriosoul <tboss1780@gmail.com>
2024-10-27 12:18:14 +0100

    update prod implementado

--- To-Dolist/main.py
+++ To-Dolist/main.py
@@ -6,22 +6,22 @@ def prod():
     return prods
 prod()
 
-# def new_prod():
-#     title = input("Nombre: ")
-#     description = input("Definicion: ")
-#     done = input("Terminado: ")
-#     ultimo_producto = products[-1]['id']
-#     nuevo_id = ultimo_producto +1
-#     nuevo_prod = {
-#         'id': nuevo_id,
-#         'title': title,
-#         'description': description,
-#         'done': done
-#     }
-#     products.append(nuevo_prod)
-#     print(products)
+def new_prod():
+    title = input("Nombre: ")
+    description = input("Definicion: ")
+    done = input("Terminado: ")
+    ultimo_producto = products[-1]['id']
+    nuevo_id = ultimo_producto +1
+    nuevo_prod = {
+        'id': nuevo_id,
+        'title': title,
+        'description': description,
+        'done': done
+    }
+    products.append(nuevo_prod)
+    print(products)
 
-# new_prod()
+new_prod()
 
 
 def kit_prod():
@@ -36,12 +36,44 @@ def kit_prod():
             if product['id'] == num_elim:
                 products.remove(product)
                 print("Se elimino correctamnete el producto", product)
-            
+                
             else:
                 print("No se encontró un producto con ese id.")
     else:
         print("No se eliminó nada")
 
 kit_prod()
+prod()
+
+
+def upd_prod():
+    print(products)
+    num_elim = int(input("Introduce el id que deseas modificar: "))  # Convertimos a entero
+    print(f"Ha seleccionado el {num_elim}")
+    opcion = input("¿Es correcto? Y(Si)/N(no)").title()
+    print(opcion)
+    
+    if opcion == "Y":
+        for product in products:
+            if product['id'] == num_elim:
+                title = input("Nombre producto: ")
+                description = input("Descripcion del producto: ")
+                done = input("Terminado: ")
+                
+                prod_upd = {
+                    'id': product['id'],
+                    'title': title,
+                    'description': description,
+                    'done': done
+                }
+                product.update(prod_upd)
+                print("Se elimino correctamnete el producto", product)
+            
+            else:
+                print("No se encontró un producto con ese id.")
+    else:
+        print("No se eliminó nada")
+
+upd_prod()
 prod()
 
