4061aed8ba2b10cd097893578dbb93c77a03e2a0
Eriosoul <tboss1780@gmail.com>
2024-10-29 01:03:51 +0100

    solucionando problema de redirecion

--- To-Dolist/app.py
+++ To-Dolist/app.py
@@ -16,7 +16,20 @@ def product():
 
 @app.route('/new_prod', methods=['POST', 'GET'])
 def new_products():
-    return f'{m.new_prod}'
+    if request.method == "POST":
+        title = request.form['title']
+        description = request.form['description']
+        done = request.form['done']
+        nuevo_id = products[-1]['id'] + 1 if products else 1
+        nuevo_prod = {
+            'id': nuevo_id,
+            'title': title,
+            'description': description,
+            'done': done
+        }
+        products.append(nuevo_prod)
+        return redirect(url_for('product'))
+    return render_template('add_prod.html', title='Add new Product')
 
     
 if __name__ == '__main__':
